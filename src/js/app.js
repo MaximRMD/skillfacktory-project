248662eab36df9a3872c0477a7e6740238f4b4a8
Maxim <maxRMD@yandex.ru>
2022-10-18 20:07:44 +0300

    final-dev

--- src/js/app.js
+++ src/js/app.js
@@ -16,3 +16,6 @@ flsSlick.slick_slider();
 
 import * as flsVideo from "./modules/play-video.js";
 flsVideo.play_video();
+
+import * as flsVideoDesktop from "./modules/play-video-desktop.js";
+flsVideoDesktop.video_desktop();
--- src/js/modules/play-video.js
+++ src/js/modules/play-video.js
@@ -14,4 +14,3 @@ export function play_video() {
         });
     });
 }
-
